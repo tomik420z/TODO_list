5c67def1e7726e1a0e2c537df8aa303a041f77ad
tomik420z <malkovartem10@gmail.com>
2023-03-23 23:44:33 +0300

    implemented writing to the data container

--- data_task.h
+++ data_task.h
@@ -13,6 +13,8 @@
 #include <unordered_map>
 #include <set>
 
+namespace property = boost::property_tree;
+
 struct data_el {
     std::string time;
     std::string task;
@@ -21,19 +23,46 @@ struct data_el {
         return time < rhs.time;
     }
 };
-    
+
 
 class data_task {
 protected:
     using ptree_t = boost::property_tree::ptree;
+    
     std::unordered_map<std::string, std::set<data_el>> set_data;
+
+    ptree_t in_root;
+    ptree_t out_root;
+
+    void fill_set_data() {
+        ptree_t list_nodes = in_root.get_child("tasks");
+        
+        for(const auto& [ingnore_path, node_values] : list_nodes) {
+        
+            std::string date = node_values.get<std::string>("date");
+            set_data[date].emplace(
+                data_el{node_values.get<std::string>("time"), node_values.get<std::string>("task")}
+                );            
+        }
+    }
+
 public:
     data_task(const char* f_name) {
-        
+        property::read_json(f_name, in_root);
+        fill_set_data();
     }
 
     ~data_task() {
 
+    }
+
+    void print_data() {
+        for(const auto& [key, value] : set_data) {
+            std::cout << "date " << key  << std::endl;
+            for(const auto& [c_time, c_task] : value) {
+                std::cout << c_time << " " << c_task << std::endl;
+            }
+        }
     }
 };
 
--- main.cpp
+++ main.cpp
@@ -1,7 +1,8 @@
 #include "data_task.h"
 
-int main() {
-
+int main(int argc, char*argv[]) {
+    data_task d(argv[1]);
+    d.print_data();
 
 
     return 0;
