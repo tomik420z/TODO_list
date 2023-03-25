9289cc92cade40f9565559535d2ded5b3b17eab4
tomik420z <malkovartem10@gmail.com>
2023-03-25 17:04:54 +0300

    modified add task function

--- data_task.h
+++ data_task.h
@@ -22,6 +22,10 @@ struct data_el {
     std::string start_time;
     std::string end_time;
     std::string task;
+
+    std::pair<std::string, std::string> get_time_interval() const {
+        return std::pair{start_time, end_time};
+    } 
     
     bool operator<(const data_el& rhs) const noexcept {
         return start_time < rhs.start_time;
@@ -58,6 +62,25 @@ protected:
         }
     }
 
+    void print_data_set(const std::set<data_el> & set) {
+        size_t ind = 0;
+        for(const auto & [start_time, end_time, task]  : set) {
+            std::cout << ++ind << ". " << start_time << "-" << end_time << std::endl;
+            std::cout << "task: " << task << std::endl;
+        }
+    }
+
+    bool check_interval(const std::string& start_interval, const std::string& end_interval, 
+                        const std::string& new_start, const std::string& new_end) {
+        if (new_end < start_interval) {
+            return true;
+        } else if (new_start > end_interval) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
 public:
     data_task(const char* f_name) : file_name(f_name) {
         property::read_json(f_name, in_root);
@@ -81,29 +104,59 @@ public:
         property::write_json(file_name, out_root);
     }
 
-    void add_new_task() {
-        std::cout << "input date : " << std::endl;
-        std::string date;
-        std::getline(std::cin, date, '\n');
-        
-        std::cout << "input task : " << std::endl;
-        std::string task;
-        std::getline(std::cin, task, '\n');
-        
-        std::cout << "input time start : " << std::endl;
-        std::string time_start;
-        std::getline(std::cin, time_start, '\n');
-        
-        
-        std::cout << "input time end : " << std::endl;
-        std::string time_end;
-        std::getline(std::cin, time_end, '\n');
-        
+    void add_new_task(std::string&& date, std::string&& task, std::string&& time_start, std::string&& time_end) {
+        if (auto it_find = set_data.find(date); it_find != set_data.end()) {
+            auto& ref_set = it_find->second;
+            
+            auto iter_next = ref_set.upper_bound(data_el{time_start, std::string{}, std::string{}});
+            bool flag_insert = true;
+            if (iter_next != ref_set.end()) {
+                const auto& [next_start, next_end] = iter_next->get_time_interval();
+                if (!check_interval(next_start, next_end, time_start, time_end)) {
+                    flag_insert = false;
+                }
+            }
 
-        set_data[date].insert(data_el{time_start, time_end, task});  
+            if (auto iter_prev = iter_next; iter_next != ref_set.begin()) {
+                --iter_prev;
+                const auto &[prev_start, prev_end] = iter_prev->get_time_interval();
+                if (!check_interval(prev_start, prev_end, time_start, time_end)) {
+                    flag_insert = false;
+                }
+            }
+
+            if (flag_insert) {
+                ref_set.insert(data_el{std::move(time_start), std::move(time_end), std::move(task)});
+            } else {
+                throw "error";
+            }
+             
+
+        } else {
+            set_data[date].insert(data_el{std::move(time_start), std::move(time_end), std::move(task)});  
+        }
     }
     
-
+    void remove(const std::string& date, size_t select_index) {
+        if (auto it_find = set_data.find(date); it_find != set_data.end()) {
+            auto& ref_set = it_find->second;
+            print_data_set(ref_set);
+            if (1 <= select_index && select_index <= ref_set.size()) {
+                auto it_erase = ref_set.begin();
+                for(size_t i = 1; i < select_index; ++i) {
+                    ++it_erase;
+                }
+                ref_set.erase(it_erase);
+                if (ref_set.empty()) {
+                    set_data.erase(it_find);
+                }
+            } else {
+                throw "task with number n is not in the list"
+            }
+        } else {
+            throw "there are no tasks for this date";
+        }
+    }
     
     ~data_task() {
 
--- main.cpp
+++ main.cpp
@@ -2,9 +2,13 @@
 
 int main(int argc, char*argv[]) {
     data_task d(argv[1]);
-    d.add_new_task();
-    d.print_data();
-    
+    try {
+        d.print_data();
+        d.add_new_task("12.03.2024", "learn c++", "11:42", "11:59");
+        
+    } catch(const char* e) {
+        std::cout << e << std::endl;
+    }
     d.write_to_json();
 
 
