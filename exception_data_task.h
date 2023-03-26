897cd0214c0b4878104c4a8d2f2b3f7440e58c2a
tomik420z <malkovartem10@gmail.com>
2023-03-26 18:26:13 +0300

    fix bug

--- exception_data_task.h
+++ exception_data_task.h
@@ -18,9 +18,9 @@ public:
     static std::string incorrect_format_interval() { return std::string("incorrect format interval"); }
 
     static std::string missing_value_in_the_list(size_t index_select) { return std::string("task with number ") + 
-                                                    std::to_string(index_select) + std::string(" is not in the list") }
+                                                    std::to_string(index_select) + std::string(" is not in the list"); }
     
-    static std::string non_existent_date() { std::string("there are no tasks for this date")  }
+    static std::string non_existent_date() { std::string("there are no tasks for this date");  }
 
     static std::string task_is_superimposed_on_another() { std::string("Can't add a task because there is already a scheduled task at that time"); } 
     
